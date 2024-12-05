621405d2fdfee9c16c0bdcb5f78c7013d2442905
Davronov-Alimardon <ormusorsen2024@gmail.com>
2024-12-05 05:15:26 +0500

    added: Tiptap extentions

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -3,6 +3,17 @@
 import { useEditor, EditorContent } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
 
+import TaskItem from "@tiptap/extension-task-item";
+import TaskList from "@tiptap/extension-task-list";
+
+import Table from "@tiptap/extension-table";
+import TableCell from "@tiptap/extension-table-cell";
+import TableHeader from "@tiptap/extension-table-header";
+import TableRow from "@tiptap/extension-table-row";
+
+import Image from '@tiptap/extension-image'
+import ImageResize from "tiptap-extension-resize-image"
+
 export const Editor = () => {
   const editor = useEditor({
     editorProps: {
@@ -12,8 +23,33 @@ export const Editor = () => {
           "focus:outline-none print:boder-0 border bg-white border-editor-border flex flex-col min-h-[1054px] w-[816px] pt-10 pr-14 pb-10 cursor-text",
       },
     },
-    extensions: [StarterKit],
-    content: "<p>Hello World! 🌎️</p>",
+    extensions: [
+      StarterKit,
+      Table,
+      TableCell,
+      TableHeader,
+      TableRow,
+      TaskList,
+      Image,
+      ImageResize,
+      TaskItem.configure({ nested: true }),
+    ],
+    content: `
+        <table>
+          <tbody>
+            <tr>
+              <th>Name</th>
+              <th colspan="3">Description</th>
+            </tr>
+            <tr>
+              <td>Cyndi Lauper</td>
+              <td>Singer</td>
+              <td>Songwriter</td>
+              <td>Actress</td>
+            </tr>
+          </tbody>
+        </table>
+      `,
   });
 
   return (
