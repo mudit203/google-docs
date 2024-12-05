cb2e7f5f730bb1aea37d084f3e1c904ed645dcef
Davronov-Alimardon <ormusorsen2024@gmail.com>
2024-12-05 11:30:42 +0500

    added:Editor Store

--- src/app/documents/[documentId]/page.tsx
+++ src/app/documents/[documentId]/page.tsx
@@ -1,14 +1,16 @@
 import { Editor } from "./editor";
+import { Toolbar } from "./toolbar";
 
 interface DocumentIdPageProps {
   params: Promise<{ documentId: string }>;
 }
 
 const DocumentIdPage = async ({ params }: DocumentIdPageProps) => {
-  // const { documentId } = await params;
+  const { documentId } = await params;
 
   return (
     <div className="min-h-screen bg-editor-bg">
+      <Toolbar />
       <Editor />
     </div>
   );
