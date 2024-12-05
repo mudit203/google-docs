ed70da5279806064ee5665b8a0df4079ca01b7ec
Davronov-Alimardon <ormusorsen2024@gmail.com>
2024-12-05 16:28:52 +0500

    updated: Align and list tools

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -21,6 +21,8 @@ import TextStyle from "@tiptap/extension-text-style";
 import { Color } from "@tiptap/extension-color";
 import Highlight from "@tiptap/extension-highlight";
 
+import TextAlign from "@tiptap/extension-text-align";
+
 import Link from "@tiptap/extension-link";
 
 import { useEditorStore } from "@/store/use-editor-store";
@@ -73,6 +75,9 @@ export const Editor = () => {
       FontFamily,
       TextStyle,
       Color,
+      TextAlign.configure({
+        types: ["heading", "paragraph"]
+      }),
       Link.configure({
         openOnClick: false,
         autolink: true,
